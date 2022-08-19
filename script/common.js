25e1b2e17ae0bb1e851c8515aedabf59bc059c18
unknown <mehedi15-2995@diu.edu.bd>
2022-08-19 20:09:31 +0600

    another

--- script/common.js
+++ script/common.js
@@ -4,12 +4,25 @@ function getTextElementValueById (elementId){
     const currentPhoneTotal = parseInt(currentPhoneTotalString);
     return currentPhoneTotal;
 }
+function setTextElementValuById (elementId,value){
+    const subTotalElement = document.getElementById(elementId);
+   subTotalElement.innerText = value;
+}
 
 function calculateSumTotal(){
        //calculate total
    const currentPhoneTotal = getTextElementValueById('phone-total')
    const currentCaseTotal = getTextElementValueById('case-total');
    const currentSubTotal = currentPhoneTotal + currentCaseTotal;
-   const subTotalElement = document.getElementById('sub-total');
-   subTotalElement.innerText = currentSubTotal;
+   setTextElementValuById('sub-total',currentSubTotal);
+   
+
+
+   //calculate tax
+   const taxAmountString = (currentSubTotal * 0.02).toFixed(2);
+   const taxAmount = parseFloat(taxAmountString);
+
+   setTextElementValuById('tax-amount',taxAmount);
+   const finalAmount = currentSubTotal+taxAmount;
+   setTextElementValuById('final-total',finalAmount);
 }
